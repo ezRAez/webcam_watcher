1752c684e3c1db100775178e6b177ddbd64df257
Ezra Raez <ezRAez10@gmail.com>
2017-01-09 14:24:28 -0800

    clean up watcher

--- motion_detector.py
+++ motion_detector.py
@@ -1,17 +1,17 @@
 import cv2, time
 
+first_frame = None
+
 video = cv2.VideoCapture(0)
 
-a = 0
-
 while True:
-    a = a + 1
-
     check, frame = video.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    #time.sleep(3)
+    if first_frame == None:
+        first_frame = gray
+
     cv2.imshow("Capturing", gray)
 
     key = cv2.waitKey(1)
@@ -20,6 +20,5 @@ while True:
     if key == ord('q'):
         break
 
-print(a)
 video.release()
 cv2.destroyAllWindows
